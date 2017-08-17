211df09cdd18043ea7781715dd58cb2fc741afc4
Nate K <kobn2801@gmail.com>
2017-08-17 07:18:37 +0200

    Generated AdminBUndle

--- src/UserBundle/Controller/LoginController.php
+++ src/UserBundle/Controller/LoginController.php
@@ -11,37 +11,37 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class LoginController extends Controller
 {
-    /**
-     * @Route("/register", name="user_register")
-     */
-    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        // 1) build the form
-        $user = new User();
-        $form = $this->createForm(RegisterType::class, $user);
+    // /**
+    //  * @Route("/register", name="user_register")
+    //  */
+    // public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    // {
+    //     // 1) build the form
+    //     $user = new User();
+    //     $form = $this->createForm(RegisterType::class, $user);
 
-        // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+    //     // 2) handle the submit (will only happen on POST)
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
 
-            // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPwd($password);
+    //         // 3) Encode the password (you could also do this via Doctrine listener)
+    //         $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+    //         $user->setPwd($password);
 
-            // 4) save the User!
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+    //         // 4) save the User!
+    //         $em = $this->getDoctrine()->getManager();
+    //         $em->persist($user);
+    //         $em->flush();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
+    //         // ... do any other work - like sending them an email, etc
+    //         // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('/app');
-        }
+    //         return $this->redirectToRoute('/app');
+    //     }
 
-        return $this->render(
-            'registration/register.html.twig',
-            array('form' => $form->createView())
-        );
-    }
+    //     return $this->render(
+    //         'registration/register.html.twig',
+    //         array('form' => $form->createView())
+    //     );
+    // }
 }
