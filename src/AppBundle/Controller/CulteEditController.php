0609e0528e80067877b60fe325ebd0a9b37eb9df
Nate K <kobn2801@gmail.com>
2017-10-21 14:30:18 +0200

    Designer done, added role

--- src/AppBundle/Controller/CulteEditController.php
+++ src/AppBundle/Controller/CulteEditController.php
@@ -43,4 +43,44 @@ class CulteEditController extends Controller
             'edit_form' => $editForm->createView()
         ));
     }
+
+    /**
+     * Lists all culte entities.
+     *
+     * @Route("/{id}/img/edit", name="app_culteedit_img")
+     * @Method({"GET", "POST"})
+     */
+    public function editImageAction(Request $request, Culte $culte) {
+        if(!$this->get('security.authorization_checker')->isGranted('ROLE_DESIGN')) {
+            throw $this->createAccessDeniedException();
+        }
+        if($culte->getBanner()) {
+            $culte->setBanner(new File($this->getParameter('message_banner_directory').'/'.$culte->getBanner()));
+        }
+        $editForm = $this->createForm('AppBundle\Form\DesignType', $culte);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            // $file stores the uploaded file
+            /** @var Symfony\Component\HttpFoundation\File\UploadedFile $file */
+            $file = $culte->getBanner();
+            if($file) {
+                $fileName = md5(uniqid()).'.'.$file->guessExtension();
+                $file->move(
+                    $this->getParameter('message_banner_directory'),
+                    $fileName
+                );
+                $culte->setBanner($fileName);
+            }
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_culte_show', array('id' => $culte->getId()));
+        }
+
+        return $this->render('culte/design_edit.html.twig', array(
+            'culte' => $culte,
+            'edit_form' => $editForm->createView()
+        ));
+    }
 }
